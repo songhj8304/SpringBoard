70c7d49eec26ead4646f0aa2b4c2f2ae61c9578f
HyeJu Song <songhj8304@gmail.com>
2025-08-04 13:03:05 +0900

    repository 생성 및 테스트

--- src/test/java/com/mysite/board/BoardApplicationTests.java
+++ src/test/java/com/mysite/board/BoardApplicationTests.java
@@ -1,13 +1,125 @@
 package com.mysite.board;
 
+import com.mysite.board.answer.Answer;
+import com.mysite.board.answer.AnswerRepository;
+import com.mysite.board.question.Question;
+import com.mysite.board.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BoardApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
 	@Test
-	void contextLoads() {
+	void 질문저장() {
+		Question q1 = new Question();
+		q1.setSubject("board가 무엇인가요?");
+		q1.setContent("board에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링 부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
 	}
 
+	@Test
+	void 질문조회1() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("board가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void 질문조회2() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("board가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	@Test
+	void 질문조회3() {
+		Question q = questionRepository.findBySubject("board가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	void 질문조회4() {
+		Question q = questionRepository.findBySubjectAndContent("board가 무엇인가요?", "board에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	void 질문수정() {
+		Optional<Question> oq = questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		questionRepository.save(q);
+	}
+
+
+	@Test
+	void 질문삭제() {
+		assertEquals(2, questionRepository.count());
+		Optional<Question> oq = questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		questionRepository.delete(q);
+		assertEquals(1, questionRepository.count());
+	}
+
+	@Test
+	void 답변저장() {
+		Optional<Question> oq = questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		answerRepository.save(a);
+	}
+
+	@Test
+	void 답변조회() {
+		Optional<Answer> oa = answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
+
+	@Test
+	@Transactional
+	void 질문으로답변조회() {
+		Optional<Question> oq = questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
