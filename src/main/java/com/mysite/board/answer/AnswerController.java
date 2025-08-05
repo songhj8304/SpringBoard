aea715af80ef0c19ac3434ec70c26655b8f167ce
HyeJu Song <songhj8304@gmail.com>
2025-08-05 10:57:58 +0900

    답변 기능 구현, 부트스트랩 적용

---
+++ src/main/java/com/mysite/board/answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.mysite.board.answer;
+
+import com.mysite.board.question.Question;
+import com.mysite.board.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content) {
+        Question question = questionService.getQuestion(id);
+        //TODO: 답변 저장
+        answerService.create(question, content);
+        return String.format("redirect:/question/detail/$s", id);
+    }
+}
