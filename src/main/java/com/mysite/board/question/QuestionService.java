13577025fd821ce7769d9f15c0b4cebf36e86a56
HyeJu Song <songhj8304@gmail.com>
2025-08-05 10:06:52 +0900

    질문목록, 상세페이지

---
+++ src/main/java/com/mysite/board/question/QuestionService.java
@@ -0,0 +1,27 @@
+package com.mysite.board.question;
+
+import com.mysite.board.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
