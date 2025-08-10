e0b0089118063635fc5be123a2759ad0395d5cf4
HyeJu Song <songhj8304@gmail.com>
2025-08-10 13:51:13 +0900

    로그인, 로그아웃 기능 구현

---
+++ src/main/java/com/mysite/board/MainController.java
@@ -0,0 +1,13 @@
+package com.mysite.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
--- src/main/java/com/mysite/board/SecurityConfig.java
+++ src/main/java/com/mysite/board/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mysite.board;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,18 +13,26 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
                 authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/**").permitAll()).
                 csrf((csrf) -> csrf.ignoringRequestMatchers("/h2-console/**"))
-                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"))
+                .logout((logout) -> logout.logoutUrl("/user/logout").logoutSuccessUrl("/").invalidateHttpSession(true));
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
